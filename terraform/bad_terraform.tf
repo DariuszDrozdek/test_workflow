660c4cbb896e6f4463de2d8f32882af4257817b2
dariusz.drozdek <dariusz.drozdek@globallogic.com>
2024-05-07 12:13:06 +0200

    add bad terraform file

---
+++ terraform/bad_terraform.tf
@@ -0,0 +1,43 @@
+# Bad Terraform script with poor conventions
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+  profile = "jack.roper"
+}
+
+variable "configuration" {
+  description = "EC2 configuration"
+  default     = [{}]
+}
+
+locals {
+  serverconfig = [
+    for srv in var.configuration : [
+      for i in range(1, srv.no_of_instances + 1) : {
+        name          = "my-instance-${i}"
+        instance_type = srv.instance_type
+        ami           = srv.ami
+      }
+    ]
+  ]
+}
+
+resource "aws_instance" "my_instances" {
+  count = length(local.serverconfig)
+
+  ami           = local.serverconfig[count.index].ami
+  instance_type = local.serverconfig[count.index].instance_type
+  key_name      = "my-keypair"
+  tags = {
+    Name = local.serverconfig[count.index].name
+  }
+}
